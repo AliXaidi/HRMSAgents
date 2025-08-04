4f49d0359077fad137193ff56fedf2ffebc1023a
ZahidHussain <zahid@swishsols.com>
2025-08-04 20:38:53 +0500

    Update server.py

--- api/server.py
+++ api/server.py
@@ -1,15 +1,3 @@
-# from fastapi import FastAPI, Request
-# from agents.attendance_agent import attendance_agent
-
-# app = FastAPI()
-
-# @app.post("/attendance")
-# async def chat(request: Request):
-#     data = await request.json()
-#     user_input = data.get("message")
-#     print('User Input:', user_input)
-#     response = attendance_agent.invoke(user_input)
-#     return {"response": response}
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
